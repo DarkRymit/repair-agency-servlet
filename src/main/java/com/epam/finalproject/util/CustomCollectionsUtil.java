dab62475cb10efba1a12ba1c307bfaafdb042f5b
Gurbanov Tamerlan <t64566t@gmail.com>
2022-08-25 17:21:19 +0300

    refactor(util.CustomCollectionsUtil) remove unused method
    Removes method toSingleton because its outdated api replaced by method toOptional in framework folder

--- src/main/java/com/epam/finalproject/util/CustomCollectionsUtil.java
+++ src/main/java/com/epam/finalproject/util/CustomCollectionsUtil.java
@@ -7,16 +7,6 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public interface CustomCollectionsUtil {
-
-    static <T> Collector<T, ?, T> toSingleton() {
-        return Collectors.collectingAndThen(Collectors.toList(), list -> {
-            if (list.size() != 1) {
-                throw new IllegalStateException();
-            }
-            return list.get(0);
-        });
-    }
-
     static <T, K, U> Collector<T, ?, Map<K, U>> toMapOfNullables(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends U> valueMapper) {
         return Collectors.collectingAndThen(Collectors.toList(), list -> {
             Map<K, U> map = new LinkedHashMap<>();
